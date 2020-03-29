8190a1855d5d412b79be38cde3a6506daeb8655a
Simon <simon.rueba@student.uibk.ac.at>
2020-03-29 15:35:42 +0200

    [Update] Added chessboards images
    This commit includes around 250 chessboard calibration images.

--- src/__main__.py
+++ src/__main__.py
@@ -9,16 +9,98 @@
 
 
 import sys
-
 import cv2
+import numpy as np
+import glob
+import os
 
-VIDEO_PATH = "resources/cube.mp4" # TODO replace with real video path
+VIDEO_PATH = "resources/testvid.MOV" # TODO replace with real video path
+CHESSBOARD_PATH = "resources/chessboard/"
+
+CHESSBOARD_X = 9
+CHESSBOARD_Y = 6
 
 def main():
     check_version()
     video = load_video(VIDEO_PATH)
     show_video(video)
-    print(sys.version)
+    chessboards()
+
+
+def chessboards():
+
+    # termination criteria
+    criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
+
+    # prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
+    objp = np.zeros((CHESSBOARD_Y * CHESSBOARD_X, 3), np.float32)
+    objp[:, :2] = np.mgrid[0:CHESSBOARD_X, 0:CHESSBOARD_Y].T.reshape(-1, 2)
+
+    # Arrays to store object points and image points from all the images.
+    objpoints = []  # 3d point in real world space
+    imgpoints = []  # 2d points in image plane.
+
+    images = glob.glob(CHESSBOARD_PATH +  '*.JPG')
+    counter = 0
+
+    for fname in images:
+        img = cv2.imread(fname)
+        print(img.shape)
+        img = cv2.resize(img, (0,0), fx=0.3, fy=0.3)
+        print(img.shape)
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        counter = counter + 1
+        if(counter > 12):
+            break
+
+        print(counter)
+        # Find the chess board corners
+        ret, corners = cv2.findChessboardCorners(gray, (CHESSBOARD_X, CHESSBOARD_Y), None)
+
+        # If found, add object points, image points (after refining them)
+        if ret == True:
+            objpoints.append(objp)
+
+            corners2 = cv2.cornerSubPix(gray, corners, (11, 11), (-1, -1), criteria)
+            imgpoints.append(corners2)
+
+            # Draw and display the corners
+            img = cv2.drawChessboardCorners(img, (CHESSBOARD_X, CHESSBOARD_Y), corners2, ret)
+            cv2.imshow("test", img)
+            cv2.waitKey(100)
+
+    print("destroying")
+    cv2.destroyAllWindows()
+    print("calc")
+    ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
+    print("idk")
+    # refine the camera matrix based on a free scaling parameter. If the scaling parameter alpha=0,
+    # it returns undistorted image with minimum unwanted pixels. So it may even remove some pixels at
+    # image corners. If alpha=1, all pixels are retained with some extra black images.
+    img = cv2.imread("resources/IMG_5983.JPG")
+    h, w = img.shape[:2]
+    newcameramtx, roi = cv2.getOptimalNewCameraMatrix(mtx, dist, (w, h), 1, (w, h))
+
+    # undistort
+    dst = cv2.undistort(img, mtx, dist, None, newcameramtx)
+
+    # crop the image, output saved as calibresult.png in the pics folder. All edges are straight.
+    x, y, w, h = roi
+    dst = dst[y:y + h, x:x + w]
+    print(os.path.sep)
+    cv2.imwrite("resources/calibresult.png", dst)
+
+    # Now you can store the camera matrix and distortion coefficients using write functions in Numpy (np.savez, np.savetxt etc) for future uses.
+    # Tutorial from https://opencv-python-tutroals.readthedocs.io/en/latest/py_tutorials/py_calib3d/py_calibration/py_calibration.html
+
+    mean_error = 0
+    for i in range(len(objpoints)):
+        # transform the object point to image point
+        imgpoints2, _ = cv2.projectPoints(objpoints[i], rvecs[i], tvecs[i], mtx, dist)
+        error = cv2.norm(imgpoints[i], imgpoints2, cv2.NORM_L2) / len(imgpoints2)
+        mean_error += error
+
+    print("total error: ", mean_error / len(objpoints))
 
 
 def show_video(cap):
@@ -101,6 +183,8 @@ def check_version():
         """
     if ("3" not in sys.version):
         print("You are running python version " + sys.version + ". We recommend python 3.7.2 for expected performance.")
+
+    print("Open CV version: " + cv2.__version__)
 
 
 if __name__ == "__main__":
