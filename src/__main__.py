bc2f49ccdabad0b766915b300c590c71d488b589
Simon <simon.rueba@student.uibk.ac.at>
2020-03-29 23:45:59 +0200

    [Update] Added drawing feature, refactor code etc...
    This commit includes new features regarding drawing an object in the video. Supplemental operations have been added and the code was partially refactored. Calibration data can now be loaded and saved in a .yaml file.

--- src/__main__.py
+++ src/__main__.py
@@ -12,22 +12,32 @@ import sys
 import cv2
 import numpy as np
 import glob
+from os.path import isfile, join
 import os
+import yaml
+from tqdm import tqdm
 
-VIDEO_PATH = "resources/testvid.MOV" # TODO replace with real video path
+VIDEO_INPUT_PATH = "resources/videos/chessboard.MOV" # TODO replace with real video path
+VIDEO_OUTPUT_PATH = "resources/vid_to_img/"
+IMG_WITH_DRAWING_PATH = "resources/img_with_drawings/"
 CHESSBOARD_PATH = "resources/chessboard/"
+CALIBRATION_PATH = "resources/calibration/calibration.yaml"
 
 CHESSBOARD_X = 9
 CHESSBOARD_Y = 6
 
+FPS = 60.0
+
 def main():
     check_version()
-    video = load_video(VIDEO_PATH)
-    show_video(video)
-    chessboards()
 
+    if (input("Do you want to calibrate the camera coefficients? This may take some time. (yes, no)") == "yes"):
+        calc_camera_calibration()
+    else:
+        chessboards()
 
-def chessboards():
+def calc_camera_calibration():
+    print("[CALIBRATION] : Calculating camera calibration...")
 
     # termination criteria
     criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
@@ -40,67 +50,160 @@ def chessboards():
     objpoints = []  # 3d point in real world space
     imgpoints = []  # 2d points in image plane.
 
-    images = glob.glob(CHESSBOARD_PATH +  '*.JPG')
+    counter = 0
+    number_of_images = len(os.listdir(VIDEO_OUTPUT_PATH)) - 1
+    with tqdm(total=number_of_images) as pbar:
+        for fname in glob.glob(CHESSBOARD_PATH + '*.JPG'):
+            counter = counter + 1
+            if (counter == number_of_images):
+                pbar.update(1)
+                break
+
+            img = cv2.imread(fname)
+            gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+            # Find the chess board corners
+            ret, corners = cv2.findChessboardCorners(gray, (CHESSBOARD_X, CHESSBOARD_Y), None)
+
+            # If found, add object points, image points (after refining them)
+            if ret == True:
+                objpoints.append(objp)
+                corners2 = cv2.cornerSubPix(gray, corners, (11, 11), (-1, -1), criteria)
+                imgpoints.append(corners2)
+
+            pbar.update(1)
+
+    cv2.destroyAllWindows()
+
+    ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
+
+    print("[CALIBRATION] : Done with calculating. Saveing file in: " + CALIBRATION_PATH)
+    # It's very important to transform the matrix to list.
+    data = {'camera_matrix': np.asarray(mtx).tolist(), 'dist_coeff': np.asarray(dist).tolist()}
+    with open(CALIBRATION_PATH, "w") as f:
+        yaml.dump(data, f)
+
+    print("[CALIBRATION] : File saved!")
+
+def get_camera_calibration():
+    print("[CALIBRATION] : Loading camera calibration file from: " + CALIBRATION_PATH)
+    with open(CALIBRATION_PATH) as f:
+        loadeddict = yaml.load(f, Loader=yaml.FullLoader)
+
+    mtxloaded = loadeddict.get('camera_matrix')
+    distloaded = loadeddict.get('dist_coeff')
+
+    print("[CALIBRATION] : Done loading.")
+
+    return mtxloaded, distloaded
+
+
+def chessboards():
+
+    # termination criteria
+    criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
+
+    # prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
+    objp = np.zeros((CHESSBOARD_Y * CHESSBOARD_X, 3), np.float32)
+    objp[:, :2] = np.mgrid[0:CHESSBOARD_X, 0:CHESSBOARD_Y].T.reshape(-1, 2)
+
+    mtx, dist = get_camera_calibration()
+    axis = get_axis("cube")
+
+    video_to_frames(VIDEO_INPUT_PATH, VIDEO_OUTPUT_PATH)
     counter = 0
 
-    for fname in images:
-        img = cv2.imread(fname)
-        print(img.shape)
-        img = cv2.resize(img, (0,0), fx=0.3, fy=0.3)
-        print(img.shape)
-        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    print("[DRAWING] : Drawing shape in every frame.")
+    for fname in tqdm(range(len(os.listdir(VIDEO_OUTPUT_PATH)) - 1)):
         counter = counter + 1
-        if(counter > 12):
-            break
-
-        print(counter)
-        # Find the chess board corners
+        img = cv2.imread(VIDEO_OUTPUT_PATH + str(counter) + '.png')
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
         ret, corners = cv2.findChessboardCorners(gray, (CHESSBOARD_X, CHESSBOARD_Y), None)
 
-        # If found, add object points, image points (after refining them)
         if ret == True:
-            objpoints.append(objp)
-
             corners2 = cv2.cornerSubPix(gray, corners, (11, 11), (-1, -1), criteria)
-            imgpoints.append(corners2)
+            _, rvecs, tvecs, inliers = cv2.solvePnPRansac(objp, corners2, np.float32(mtx), np.float32(dist))
+            imgpts, jac = cv2.projectPoints(axis, rvecs, tvecs, np.float32(mtx), np.float32(dist))
+            img = draw_cube(img, corners2, imgpts)
+            cv2.imwrite(IMG_WITH_DRAWING_PATH + "{:05d}.png".format(counter), img)
 
-            # Draw and display the corners
-            img = cv2.drawChessboardCorners(img, (CHESSBOARD_X, CHESSBOARD_Y), corners2, ret)
-            cv2.imshow("test", img)
-            cv2.waitKey(100)
-
-    print("destroying")
+    print("[DRAWING] : Done.")
+    convert_frames_to_video(IMG_WITH_DRAWING_PATH, "resources/video/movie.avi", FPS)
     cv2.destroyAllWindows()
-    print("calc")
-    ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
-    print("idk")
-    # refine the camera matrix based on a free scaling parameter. If the scaling parameter alpha=0,
-    # it returns undistorted image with minimum unwanted pixels. So it may even remove some pixels at
-    # image corners. If alpha=1, all pixels are retained with some extra black images.
-    img = cv2.imread("resources/IMG_5983.JPG")
-    h, w = img.shape[:2]
-    newcameramtx, roi = cv2.getOptimalNewCameraMatrix(mtx, dist, (w, h), 1, (w, h))
 
-    # undistort
-    dst = cv2.undistort(img, mtx, dist, None, newcameramtx)
 
-    # crop the image, output saved as calibresult.png in the pics folder. All edges are straight.
-    x, y, w, h = roi
-    dst = dst[y:y + h, x:x + w]
-    print(os.path.sep)
-    cv2.imwrite("resources/calibresult.png", dst)
+def convert_frames_to_video(pathIn,pathOut,fps):
+    frame_array = []
+    files = [f for f in os.listdir(pathIn) if isfile(join(pathIn, f))]
 
-    # Now you can store the camera matrix and distortion coefficients using write functions in Numpy (np.savez, np.savetxt etc) for future uses.
-    # Tutorial from https://opencv-python-tutroals.readthedocs.io/en/latest/py_tutorials/py_calib3d/py_calibration/py_calibration.html
+    #for sorting the file names properly
+    files.sort(key = lambda x: x[:-3])
 
-    mean_error = 0
-    for i in range(len(objpoints)):
-        # transform the object point to image point
-        imgpoints2, _ = cv2.projectPoints(objpoints[i], rvecs[i], tvecs[i], mtx, dist)
-        error = cv2.norm(imgpoints[i], imgpoints2, cv2.NORM_L2) / len(imgpoints2)
-        mean_error += error
+    print("[CONVERSION] : Converting " + str(len(files)) + " images to video.")
 
-    print("total error: ", mean_error / len(objpoints))
+    for i in tqdm(range(len(files))):
+        filename=pathIn + files[i]
+        img = cv2.imread(filename)
+        height, width, layers = img.shape
+        frame_array.append(img)
+
+    # fourcc = cv2.cv.CV_FOURCC(*'mp4v')
+    #fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+    fourcc = cv2.VideoWriter_fourcc(*"XVID")
+    videowriter = cv2.VideoWriter("output.avi", fourcc, 30.0, (width, height))
+
+    for i in range(0, len(frame_array) - 1):
+        videowriter.write(frame_array[i])
+    videowriter.release()
+
+    print("[CONVERSION] : Video saved in: " + pathOut)
+
+
+def get_axis(desc):
+
+    if(desc == 'cube'):
+        return np.float32([[0, 0, 0], [0, 3, 0], [3, 3, 0], [3, 0, 0], [0, 0, -3], [0, 3, -3], [3, 3, -3], [3, 0, -3]])
+    elif(desc == 'coord'):
+        return np.float32([[3, 0, 0], [0, 3, 0], [0, 0, -3]]).reshape(-1, 3)
+    else:
+        print(desc + " is not valid, only cube or coord are possible.")
+
+
+def draw(img, corners, imgpts):
+    corner = tuple(corners[0].ravel())
+    img = cv2.line(img, corner, tuple(imgpts[0].ravel()), (255, 0, 0), 5)
+    img = cv2.line(img, corner, tuple(imgpts[1].ravel()), (0, 255, 0), 5)
+    img = cv2.line(img, corner, tuple(imgpts[2].ravel()), (0, 0, 255), 5)
+    return img
+
+
+def draw_cube(img, corners, imgpts):
+    imgpts = np.int32(imgpts).reshape(-1, 2)
+    img = cv2.drawContours(img, [imgpts[:4]], -1, (0,255,0), -3)
+
+    for i,j in zip(range(4), range(4,8)):
+        img = cv2.line(img, tuple(imgpts[i]), tuple(imgpts[j]),(255),3)
+
+    img = cv2.drawContours(img, [imgpts[4:]],-1,(0,0,255),3)
+
+    return img
+
+
+def video_to_frames(video, path_output_dir):
+    # extract frames from a video and save to directory as 'x.png' where
+    # x is the frame index
+    vidcap = load_video(VIDEO_INPUT_PATH)
+    count = 0
+
+    while vidcap.isOpened():
+        success, image = vidcap.read()
+        if success:
+            cv2.imwrite(os.path.join(path_output_dir, '%d.png') % count, image)
+            count += 1
+        else:
+            break
+    cv2.destroyAllWindows()
+    vidcap.release()
 
 
 def show_video(cap):
@@ -165,7 +268,7 @@ def load_video(path):
         print("Error loading video file from path: " + path)
         sys.exit(-1)
     else:
-        print("Succesfully loaded video from path: " + path)
+        print("[VIDEO] : Succesfully loaded video from path: " + path)
 
     return video
 
